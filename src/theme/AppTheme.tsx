d0b1860e016a9ab8f0be7f38903329d87c24a97b
Lea Muñoz <munozleandrom96@gmail.com>
2024-06-12 10:47:22 -0300

    Primer commit

---
+++ src/theme/AppTheme.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { purpleTheme } from './purpleTheme';
+
+export const AppTheme = ({ children }: any) => {
+  return (
+    <ThemeProvider theme={ purpleTheme } >
+
+        <CssBaseline />
+        
+        { children }
+    </ThemeProvider>
+  )
+}
